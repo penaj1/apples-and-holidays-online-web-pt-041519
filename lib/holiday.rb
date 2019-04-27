5ccd9d62bb42e60bf0e8382eb0709ad47fd027a7
Janice Pena <jipena94@gmail.com>
2019-04-27 18:29:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,7 @@ def all_holidays_with_bbq(holiday_hash)
  
  holiday_hash.each do |season, holiday|
    holiday.each do |holiday, supply|
-     holiday 
+     holiday if item
        end
      end
    end
