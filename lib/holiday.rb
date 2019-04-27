fcd820483f31459e8c1bffb5078c4ece0e6babc7
Janice Pena <jipena94@gmail.com>
2019-04-27 18:32:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,6 +69,7 @@ def all_holidays_with_bbq(holiday_hash)
    holiday.map do |holiday, supply|
      holiday if supply.include?("BBQ")
        end
+       holiday_hash.flatten.compact
      end.flatten.compact
    end
 
